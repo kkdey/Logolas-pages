3485dd10b69ea77ebbed0edb8864102fb26dd988
Kushal K Dey <kshldey@gmail.com>
2017-12-08 10:20:01 +0530

    removed atSNP dependency from examples

--- docs/gallery_codes/app_TFBS.R
+++ docs/gallery_codes/app_TFBS.R
@@ -1,10 +1,10 @@
 library(Logolas)
-library(atSNP)
-pfm_mk=LoadMotifLibrary('http://compbio.mit.edu/encode-motifs/motifs.txt',tag = ">",transpose = F, field = 1, sep = c("\t", " ", ">"), skipcols = 1, skiprows = 1, pseudocount = 0)
-EBF1_disc1=t(pfm_mk[[52]]);rownames(EBF1_disc1)=c('A','C','G','T');colnames(EBF1_disc1)=1:10
+out <- suppressWarnings(suppressMessages(get(load(system.file("extdata", "EBF1.RData",
+                                                              package = "Logolas")))))
 
-color_profile <- list("type" = "per_row",
-                      "col" = RColorBrewer::brewer.pal(4,name ="Spectral"))
+color_profile = list("type" = "per_row",
+                     "col" = RColorBrewer::brewer.pal(4,name ="Spectral"))
+EBF1_disc1=t(out);rownames(EBF1_disc1)=c('A','C','G','T');colnames(EBF1_disc1)=1:ncol(EBF1_disc1)
 
 nlogomaker(EBF1_disc1,logoheight = 'log',
            color_profile = color_profile,
--- docs/gallery_codes/enrichment_logo_visual.R
+++ docs/gallery_codes/enrichment_logo_visual.R
@@ -1,12 +1,11 @@
 library(Logolas)
 #read all the motif data from http://compbio.mit.edu/encode-motifs/motifs.txt
-library(atSNP)
-pfm=LoadMotifLibrary('http://compbio.mit.edu/encode-motifs/motifs.txt',tag = ">",
-                     transpose = F, field = 1, sep = c("\t", " ", ">"), skipcols = 1, skiprows = 1,
-                     pseudocount = 0)
+out <- suppressWarnings(suppressMessages(get(load(system.file("extdata", "EBF1.RData",
+                                                              package = "Logolas")))))
+
 color_profile = list("type" = "per_row",
                      "col" = RColorBrewer::brewer.pal(4,name ="Spectral"))
-EBF1_disc1=t(pfm[[52]]);rownames(EBF1_disc1)=c('A','C','G','T');colnames(EBF1_disc1)=1:ncol(EBF1_disc1)
+EBF1_disc1=t(out);rownames(EBF1_disc1)=c('A','C','G','T');colnames(EBF1_disc1)=1:ncol(EBF1_disc1)
 
 
 Logolas::nlogomaker(EBF1_disc1, logoheight = 'log',
